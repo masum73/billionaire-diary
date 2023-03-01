decb8b3b7adb81a552e82130d4f90bb630d2111a
Masum Rahman <masumrahman73@gmail.com>
2023-03-01 18:36:21 +0600

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
-const loadData = () => {
-    fetch('./data/AllBillionaires.json')
-        .then(res => res.json())
-        .then(data => displayData(data.slice(0, 25)))
-}
+const loadData = async () => {
+    const URL = `https://raw.githubusercontent.com/Chayti/Billionaire-Diary-resources/main/AllBillionaires.json`;
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+        //console.log(data.slice(0,30));
+        displayData(data.slice(0, 30));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+/*   const setInnerHtml = (data) => {
+    const tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    let serial = 1;
+    // let totalField = document.getElementById('total');
+    // let amount = 0;
+    data.forEach((billionaire) => {
+      const { person, countryOfCitizenship, industries, position, finalWorth } =
+        billionaire;
+      tbody.innerHTML += `
+          <tr data-bs-toggle="modal" data-bs-target="#billionairesDetails">
+          <th scope="row">${serial++}</th>
+          <td>${person.name}</td>
+          <td>${countryOfCitizenship}</td>
+          <td>${industries}</td>
+          <td>${position}</td>
+          <td>$${finalWorth}</td>
+        </tr>
+          `;
+          // amount += finalWorth;
+        });
+        // totalField.innerText = '$'+ amount.toFixed(2);
+  
+        // tbody.addEventListener('click', (e) => {
+        //   console.log(e.target.parentNode);
+        // })
+  }; */
 
 const displayData = (data) => {
     const tableBody = document.getElementById('tbody');
@@ -13,7 +47,7 @@ const displayData = (data) => {
         tableBody.classList.add('text');
         //console.log(billionaire.naturalId);
         tableBody.innerHTML += `
-        <tr>
+        <tr data-bs-toggle="modal" data-bs-target="#billionairesDetails">
             <th scope="row">${serial++}</th>
             <td>${personName}</td>
             <td>${countryOfCitizenship}</td>
@@ -21,21 +55,30 @@ const displayData = (data) => {
             <td>${rank}</td>
             <td>$${finalWorth}</td>
             <td>${state ? state : 'Not found'}</td>
-            <td><button onclick="openModal('${billionaire.naturalId}')" class="btn bg-white text-dark" data-bs-toggle="modal" data-bs-target="#billionaireDetailModal">Click for Details</button></td>
         </tr>
         `
     });
 
 }
 
-const openModal = (value) => {
-    console.log(value);
-};
+document.getElementById("technology-btn").addEventListener("click", async () => {
+        const URL = `https://raw.githubusercontent.com/Chayti/Billionaire-Diary-resources/main/ByIndustryTechnology.json`;
+        try {
+            const res = await fetch(URL);
+            const data = await res.json();
+            displayData(data.slice(0, 30));
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-// load state button data 
-
-const loadState = () => {
-    fetch('./data/ByStateTexas.json')
-        .then(res => res.json())
-        .then(data => displayData(data.slice(0, 15)))
-}
+document.getElementById("state-btn").addEventListener("click", async () => {
+    const URL = `https://raw.githubusercontent.com/Chayti/Billionaire-Diary-resources/main/ByStateTexas.json`;
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+        displayData(data.slice(0, 30));
+    } catch (error) {
+        console.log(error);
+    }
+});
