87ff369fcc8b28c9545a4be29d277cb9bc04b02e
Masum Rahman <masumrahman73@gmail.com>
2023-03-01 18:56:43 +0600

    refactor code with URL and JS

--- js/app.js
+++ js/app.js
@@ -3,40 +3,11 @@ const loadData = async () => {
     try {
         const res = await fetch(URL);
         const data = await res.json();
-        //console.log(data.slice(0,30));
         displayData(data.slice(0, 30));
     } catch (error) {
         console.log(error);
     }
 };
-
-/*   const setInnerHtml = (data) => {
-    const tbody = document.getElementById("tbody");
-    tbody.innerHTML = "";
-    let serial = 1;
-    // let totalField = document.getElementById('total');
-    // let amount = 0;
-    data.forEach((billionaire) => {
-      const { person, countryOfCitizenship, industries, position, finalWorth } =
-        billionaire;
-      tbody.innerHTML += `
-          <tr data-bs-toggle="modal" data-bs-target="#billionairesDetails">
-          <th scope="row">${serial++}</th>
-          <td>${person.name}</td>
-          <td>${countryOfCitizenship}</td>
-          <td>${industries}</td>
-          <td>${position}</td>
-          <td>$${finalWorth}</td>
-        </tr>
-          `;
-          // amount += finalWorth;
-        });
-        // totalField.innerText = '$'+ amount.toFixed(2);
-  
-        // tbody.addEventListener('click', (e) => {
-        //   console.log(e.target.parentNode);
-        // })
-  }; */
 
 const displayData = (data) => {
     const tableBody = document.getElementById('tbody');
@@ -45,13 +16,12 @@ const displayData = (data) => {
     data.forEach(billionaire => {
         const { personName, countryOfCitizenship, rank, industries, finalWorth, state } = billionaire;
         tableBody.classList.add('text');
-        //console.log(billionaire.naturalId);
         tableBody.innerHTML += `
         <tr data-bs-toggle="modal" data-bs-target="#billionairesDetails">
             <th scope="row">${serial++}</th>
             <td>${personName}</td>
             <td>${countryOfCitizenship}</td>
-            <td>${industries[0]}</td>
+            <td>${industries}</td>
             <td>${rank}</td>
             <td>$${finalWorth}</td>
             <td>${state ? state : 'Not found'}</td>
